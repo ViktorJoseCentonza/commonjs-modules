2ecf6562245064277e3b9d20921fa0caf3e25f59
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-03-10 13:02:17 +0100

    add function for hobbies.js

---
+++ hobbies.js
@@ -0,0 +1,9 @@
+
+
+
+function userHobbies(hobbyOne, hobbyTwo, hobbyThree) {
+    const hobbies = { hobbyOne, hobbyTwo, hobbyThree }
+    return hobbies
+}
+
+console.log(userHobbies("primo", "secondo", "terzo"));
